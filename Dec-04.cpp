1bda0b1004ad0bc1c49a0dcc476e848b75a54990
Ritesh Kushwaha <riteshkushwaha497@gmail.com>
2024-12-04 23:03:04 +0530

    Create Dec-04.cpp

---
+++ Dec-04.cpp
@@ -0,0 +1,16 @@
+
+// 2825. Make String a Subsequence Using Cyclic Increments
+
+
+
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int j = 0;
+        for(int i = 0; i<str1.size();i++){
+            if(str1[i]==str2[j] || ((str1[i]-'a'+1)%26+'a'==str2[j])) j++;
+        }
+        if(j>=str2.size()) return true;
+        else return false;
+    }
+};
