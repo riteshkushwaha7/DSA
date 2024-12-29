660c43219936bd6bb57fa852bb2437a5c894677a
Ritesh Kushwaha <riteshkushwaha497@gmail.com>
2024-12-29 22:03:54 +0530

    Create Candy.cpp

---
+++ Candy.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int candy(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        int sum = 1, i = 1;
+        while(i<n){
+            if(nums[i]==nums[i-1]){
+                sum++;
+                i++;
+                continue;
+            } 
+            int peak = 1;
+            while(i<n && nums[i]>nums[i-1]){
+                peak++;
+                sum+=peak;
+                i++;
+            }
+            int down = 1;
+            while(i<n && nums[i]<nums[i-1]){
+                sum+=down;
+                i++;
+                down++;
+            }
+            if(down>peak) sum+=down-peak;
+        }
+       return sum;
+    }
+};
